be3ea175045fdd44c010082d8159e7b9abbfde50
Tommi Järvinen <tommi.tiere@gmail.com>
2016-09-01 20:41:50 +0300

    Add some content to topbar

--- resources/views/shared/topbar.blade.php
+++ resources/views/shared/topbar.blade.php
@@ -1,3 +1,22 @@
-<nav class="navbar navbar-inverse">
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button"
+                    class="navbar-toggle collapsed"
+                    data-toggle="collapse"
+                    data-target="#navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
 
+            <a href="#" class="navbar-brand">Solid Octo Train</a>
+        </div>
+
+        <div id="navbar-collapse" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+            <li><a href="">Accounts</a></li>
+            </ul>
+        </div>
+    </div>
 </nav>
