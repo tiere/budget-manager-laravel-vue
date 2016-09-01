de2c788bc0d984471129f72abd00e557042c2554
Tommi Järvinen <tommi.tiere@gmail.com>
2016-09-01 20:31:04 +0300

    Add viewport meta tag and improve layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,11 +2,18 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
     {!! Html::style('css/app.css') !!}
+
     <title>Solid Octo Train - @yield('title')</title>
 </head>
 <body>
-    @yield('content')
+    @include('shared.topbar')
+
+    <div class="container-fluid">
+        @yield('content')
+    </div>
 
     {!! Html::script('js/app.js') !!}
 </body>
