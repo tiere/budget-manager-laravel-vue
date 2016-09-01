9ebbf40db7310bbedbd62818b8f0348b06d9783e
Tommi Järvinen <tommi.tiere@gmail.com>
2016-09-01 19:34:42 +0300

    Add name

--- database/migrations/2016_09_01_153101_create_accounts_table.php
+++ database/migrations/2016_09_01_153101_create_accounts_table.php
@@ -14,6 +14,7 @@ class CreateAccountsTable extends Migration
     public function up()
     {
         Schema::create('accounts', function (Blueprint $table) {
+            $table->string('name');
             $table->increments('id');
             $table->timestamps();
         });
