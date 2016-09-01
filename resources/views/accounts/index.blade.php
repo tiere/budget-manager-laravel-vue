8302c036c20802916c4aa28b31e4ff2547c045bf
Tommi Järvinen <tommi.tiere@gmail.com>
2016-09-01 21:02:54 +0300

    Display accounts

--- resources/views/accounts/index.blade.php
+++ resources/views/accounts/index.blade.php
@@ -2,4 +2,24 @@
 
 @section('content')
     <h1>Accounts</h1>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Name</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            @forelse ($accounts as $account)
+                <tr>
+                    <td>{{ $account->name }}</td>
+                </tr>
+                @empty
+                    <tr>
+                        <td>No accounts</td>
+                    </tr>
+                @endforelse
+        </tbody>
+    </table>
 @endsection
