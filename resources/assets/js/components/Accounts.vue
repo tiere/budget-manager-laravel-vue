ea0b301973bf6a37ee3fd9df24de4dbee1603263
Tommi Järvinen <tommi.tiere@gmail.com>
2016-09-03 14:48:20 +0300

    Replace account table with vue component

---
+++ resources/assets/js/components/Accounts.vue
@@ -0,0 +1,35 @@
+<template>
+    <h1>Accounts</h1>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Name</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr v-for="account in accounts">
+                <td>{{ account.name }}</td>
+                <td><a href="/accounts/{{ account.id }}/destroy" class="btn btn-danger">Delete</a></td>
+            </tr>
+        </tbody>
+    </table>
+
+    <a href="/accounts/create" class="btn btn-primary" tabindex="1">New account</a>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            accounts: []
+        };
+    },
+    created () {
+        $.get('api/accounts').done(data => {
+            this.accounts = data;
+        });
+    }
+};
+</script>
--- resources/views/accounts/index.blade.php
+++ resources/views/accounts/index.blade.php
@@ -1,30 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Accounts</h1>
-
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Name</th>
-            </tr>
-        </thead>
-
-        <tbody>
-            @forelse ($accounts as $account)
-                <tr>
-                    <td>{{ $account->name }}</td>
-                    <td>
-                        {!! link_to_route('accounts.destroy', 'Delete', [$account->id], ['class' => 'btn btn-danger']) !!}
-                    </td>
-                </tr>
-                @empty
-                    <tr>
-                        <td>No accounts</td>
-                    </tr>
-                @endforelse
-        </tbody>
-    </table>
-
-    {!! link_to_route('accounts.create', 'New accout', [], ['class' => 'btn btn-primary', 'tabindex' => 1]) !!}
+    <accounts></accounts>
 @endsection
