703030118c2ea282cd001ba74770cde4819e9f20
Tommi Järvinen <tommi.tiere@gmail.com>
2016-09-04 14:30:37 +0300

    Add Account vue component

---
+++ resources/assets/js/components/Account.vue
@@ -0,0 +1,16 @@
+<template>
+    <button class="btn btn-default" v-on:click.prevent="backToAccounts">Back to accounts</button>
+
+    <h1>{{ account.name }}</h1>
+</template>
+
+<script>
+    export default {
+        props: ['account'],
+        methods: {
+            backToAccounts () {
+                this.$dispatch('showing-account', false);
+            }
+        }
+    };
+</script>
